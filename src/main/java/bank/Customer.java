3863abda32fc658347aa28a1c0683e3047b8f309
Vaishnavi <59247538+vaishnavig999@users.noreply.github.com>
2024-05-31 09:40:29 +0000

    Added authenticated variable and getter setters

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -6,6 +6,7 @@ public class Customer {
   private String password;
   private int accountId;
   private String name;
+  private boolean authenticated;
 
   public Customer(int id, String name, String username, String password, int accountId) {
     setId(id);
@@ -13,6 +14,7 @@ public class Customer {
     setUsername(username);
     setPassword(password);
     setAccountId(accountId);
+    setAuthenticated(false);
   }
 
   public int getId() {
@@ -54,4 +56,13 @@ public class Customer {
   public void setName(String name) {
     this.name = name;
   }
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
+  }
+  
 }
