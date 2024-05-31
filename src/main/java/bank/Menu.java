f079b68638e5b655eb5c4bf205e7eb4528774e16
Vaishnavi <59247538+vaishnavig999@users.noreply.github.com>
2024-05-31 10:51:14 +0000

    created menu in authenticateUser method

---
+++ src/main/java/bank/Menu.java
@@ -0,0 +1,40 @@
+package bank;
+
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+public class Menu {
+  private Scanner scanner;
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Globe Bank International!");
+
+    Menu menu = new Menu();
+    menu.scanner = new Scanner(System.in);
+
+    Customer customer = menu.authenticateUser();
+    if(customer != null){
+      Account account = DataSource.getAccount(customer.getAccountId());
+      //menu.showMenu(customer, account);
+    }
+    menu.scanner.close();
+  }
+  private Customer authenticateUser(){
+    System.out.println("Please enter your username: ");
+    String username = scanner.next();
+
+    System.out.println("Please enter your password: ");
+    String password = scanner.next();
+
+    Customer customer = null;
+    try{
+      customer = Authenticator.login(username,password);
+    }catch(LoginException e){
+      System.out.println("It's an error: "+e.getMessage());
+    }
+    return customer;
+  }
+  
+  }
+
