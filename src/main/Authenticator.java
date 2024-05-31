cbfeab8a8b339af066ec2f7f22b00bd6c32f60b1
Vaishnavi <59247538+vaishnavig999@users.noreply.github.com>
2024-05-31 09:38:43 +0000

    Create Authenticator.java

---
+++ src/main/Authenticator.java
@@ -0,0 +1,21 @@
+import javax.security.auth.login.LoginException;
+
+import bank.Customer;
+import bank.DataSource;
+
+public class Authenticator {
+  public static Customer login(String username, String password) throws LoginException{
+    Customer customer = DataSource.getCustomer(username);
+    if(customer == null){
+      throw new LoginException("Username not found");
+    }
+    if(password.equals(customer.getPassword())){
+      customer.setAuthenticated(true);
+      return customer;
+    }
+    else throw new LoginException("Incorrect Password");
+  }
+  public static void logout(Customer customer){
+    customer.setAuthenticated(false);
+  }
+}
