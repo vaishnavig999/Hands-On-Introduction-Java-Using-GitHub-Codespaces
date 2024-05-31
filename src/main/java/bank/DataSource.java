f643092712d2ffb5b920c34616e13250224b4526
Vaishnavi <59247538+vaishnavig999@users.noreply.github.com>
2024-05-31 09:01:56 +0000

    Construct Account from DataSource & print result

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -14,7 +14,7 @@ public class DataSource {
 
     try {
       connection = DriverManager.getConnection(db_file);
-      System.out.println("We're connected");
+      //System.out.println("We're connected");
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -43,9 +43,28 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int account_id){
+      String sql = "select * from Accounts where id = ?";
+      Account account = null;
+      try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+          statement.setInt(1, account_id);
+
+          try(ResultSet resultSet = statement.executeQuery()){
+              account = new Account(
+                resultSet.getInt("id"),
+                resultSet.getString("type"),
+                resultSet.getDouble("balance"));
+          }
+        }catch(SQLException e){
+          e.printStackTrace();
+        }
+    return account;
+  }
   public static void main(String[] args) {
     // connect();
     Customer customer = getCustomer("telloy3x@bigcartel.com");
-    System.out.println(customer.getName());
+    Account account = getAccount(customer.getAccountId());
+    System.out.println(customer.getName() + " " + account.getBalance());
   }
 }
